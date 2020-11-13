ad4cee983dffb21c8d9c7f55082d8ba2e8e23829
Philipp Muens <raute1337@gmx.de>
2020-11-13 19:16:24 +0100

    Implement Exchange class

---
+++ classes/Exchange.js
@@ -0,0 +1,15 @@
+class Exchange {
+  constructor(ctx) {
+    this._ctx = ctx;
+  }
+
+  get ctx() {
+    return this._ctx;
+  }
+
+  async getRate(fromToken, toToken, amount) {
+    throw new Error("Not Implemented...");
+  }
+}
+
+module.exports = Exchange;
