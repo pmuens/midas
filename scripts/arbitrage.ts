27a54f8e43ddd68136bfa441fcbc6e25642406e7
Philipp Muens <raute1337@gmx.de>
2020-11-18 14:51:27 +0100

    Simplify arbitrage script

--- scripts/arbitrage.ts
+++ scripts/arbitrage.ts
@@ -14,7 +14,7 @@ const privateKey = process.env.PRIVATE_KEY as string
 
 async function main() {
   const { toBN, toWei } = hre.web3.utils
-  const { Contract, accounts, getGasPrice, sendTransaction } = hre.web3.eth
+  const { Contract, accounts } = hre.web3.eth
   const { sources, artifacts } = hre.config.paths
   const account = accounts.wallet.add(privateKey)
   const contracts = loadContracts()
@@ -22,43 +22,27 @@ async function main() {
   const amount = '1'
 
   // --- AaveFlashLoanUniswapV1 ---
-  const name = 'AaveFlashLoanUniswapV1'
   const UNISWAP_FACTORY_A_ADDRESS = '0xECc6C0542710a0EF07966D7d1B10fA38bbb86523'
   const UNISWAP_FACTORY_B_ADDRESS = '0x54Ac34e5cE84C501165674782582ADce2FDdc8F4'
-
-  const { address } = await loadDeployment(name)
-  const { abi } = await loadArtifact(sources, artifacts, name)
-
-  const instance = new Contract(abi, address)
   const context = hre.web3.eth.abi.encodeParameters(
     ['address', 'address'],
     [UNISWAP_FACTORY_A_ADDRESS, UNISWAP_FACTORY_B_ADDRESS]
   )
+  const name = 'AaveFlashLoanUniswapV1'
+  const { address } = await loadDeployment(name)
+  const { abi } = await loadArtifact(sources, artifacts, name)
 
-  const tx = instance.methods.initFlashLoan(
-    context,
-    contracts.tokens.DAI.address,
-    contracts.tokens.BAT.address,
-    toBN(toWei(amount))
-  )
+  const instance = new Contract(abi, address)
+  const tx = await instance.methods
+    .initFlashLoan(
+      context,
+      contracts.tokens.DAI.address,
+      contracts.tokens.BAT.address,
+      toBN(toWei(amount))
+    )
+    .send({ from: account.address, gas: GAS })
 
-  const [gasPrice, gas] = await Promise.all([
-    getGasPrice(),
-    tx.estimateGas({ from: account.address, gas: GAS })
-  ])
-
-  const data = tx.encodeABI()
-  const txData = {
-    from: account.address,
-    to: address,
-    data,
-    gas,
-    gasPrice
-  }
-
-  const receipt = await sendTransaction(txData)
-
-  log(`Arbitrage via "${name}" succeeded: ${receipt.transactionHash}`)
+  log(`Arbitrage via "${name}" succeeded: ${tx.transactionHash}`)
 }
 
 main()
